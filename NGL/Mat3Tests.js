25cda5200de6f152f9f2b9774825329eafa656fe
jmacey <jmacey@bournemouth.ac.uk>
2020-06-06 08:47:50 +0100

    re-factored test code and got exception testing working

--- NGL/Mat3Tests.js
+++ NGL/Mat3Tests.js
@@ -5,9 +5,9 @@ const expect  = require('chai').expect;
 const chai = require('chai');
 const Mat3 = require('./Mat3')
 
-describe('Mat3', function() {
-  describe('constructor', function() {
-    it('default Mat3 Identity', function() {
+describe('Mat3', () => {
+  describe('constructor', () => {
+    it('default Mat3 Identity', () => {
       let vec = new Mat3();
       assert.equal(vec.m[0],1.0);
       assert.equal(vec.m[1],0.0);
@@ -21,9 +21,9 @@ describe('Mat3', function() {
     });
   });
  
-  describe('Mat3', function() {
-    describe('equals', function() {
-      it('test equals function', function() {
+  describe('Mat3', () => {
+    describe('equals', () => {
+      it('test equals function', () => {
         let a = new Mat3();
         let b = new Mat3();
         assert(a.equals(b));
@@ -31,9 +31,9 @@ describe('Mat3', function() {
     });
 });
 
-describe('Mat3', function() {
-  describe('not equals', function() {
-    it('test equals fails function', function() {
+describe('Mat3', () => {
+  describe('not equals', () => {
+    it('test equals fails function', () => {
       let a = new Mat3();
       let b = new Mat3();
       b.set([1,2,3,4,5,6,7,8,9]);
@@ -42,9 +42,9 @@ describe('Mat3', function() {
   });
 });
 
-describe('Mat3', function() {
-  describe('set', function() {
-    it('test set and equals', function() {
+describe('Mat3', () => {
+  describe('set', () => {
+    it('test set and equals', () => {
       let a = new Mat3();
       let b = new Mat3();
       a.set([1,2,3,4,5,6,7,8,9]);
@@ -54,18 +54,20 @@ describe('Mat3', function() {
   });
 });
 
-describe('Mat3', function() {
-  describe('set throws', function() {
-    it('set throws', function() {
-      let a = new Mat3();
-      chai.expect( a.set([1,2,3,4,5,6]) ).to.throw('Mat3.set takes 9 float parameters')
+describe('Mat3',()=> {
+  describe('set throws', ()=> {
+    it('set throws', ()=> {
+      chai.expect( ()=>{ 
+              let a = new Mat3();
+             a.set([1,2,3,4,5,6]);
+      }).to.throw();
     });
   });
 });
 
-describe('Mat3', function() {
-  describe('identity', function() {
-    it('set to identity', function() {
+describe('Mat3', () => {
+  describe('identity', () => {
+    it('set to identity', () => {
       let a = new Mat3();
       a.set([1,2,3,4,5,6,7,8,9]);
       a.identity();
